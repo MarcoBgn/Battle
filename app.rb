9366c0af95be2ef32ecedda614115704355f7cd5
Marco Bagnasco <admin@hecticmojo.com>
2016-02-04 10:11:06 +0000

    Refactored Post/Redirect/Get

--- app.rb
+++ app.rb
@@ -2,16 +2,26 @@ require 'sinatra/base'
 require 'capybara/dsl'
 
 class Battle < Sinatra::Base
+  
+  enable :sessions
+  set :sessions, :expire_after => 5
+  
   get '/' do
     erb :names
   end
   
   post '/main' do
-    @player1 = params['player_1_name']
-    @player2 = params['player_2_name']
+    session['player_1_name'] = params['player_1_name']
+    session['player_2_name'] = params['player_2_name']
+    redirect '/play'
+  end
+  
+  get '/play' do
+    p session
+    @player1 = session['player_1_name']
+    @player2 = session['player_2_name']
     erb :main
   end
 
-  # start the server if ruby file executed directly
   run! if app_file == $0
 end
